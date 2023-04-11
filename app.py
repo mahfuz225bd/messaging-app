f7ada68044beb796129a035b1d870b73d7e3143a
mahfuz225bd <mahfuz225bd@gmail.com>
2023-04-11 20:49:23 +0600

    get usernames dynamically from server

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    usernames = ['Nick', 'Robert', 'Deondre', 'John Doe',
+                 'Jane Doe', 'Mark Smith', 'Sarah Johnson']
+    return render_template("index.html", usernames=usernames)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
